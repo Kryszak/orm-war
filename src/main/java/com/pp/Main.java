90d867ba18ba6196783c8f2fe31e62545017e516
Piotr Skonieczny <piotr.skonieczny@gmail.com>
2018-05-29 19:01:44 +0200

    Main

--- src/main/java/com/pp/Main.java
+++ src/main/java/com/pp/Main.java
@@ -1,20 +1,8 @@
 package com.pp;
 
-import com.pp.mybatis.MyBatisDAO;
-import com.pp.mybatis.migration.MybatisDataMigrationTool;
-
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
-        MybatisDataMigrationTool migrationTool = new MybatisDataMigrationTool();
-        MyBatisDAO myBatisDAO = new MyBatisDAO();
-
-        migrationTool.migrateData();
-        myBatisDAO.test();
+    public static void main(String[] args) throws Exception {
 
     }
-
 }
